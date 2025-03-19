e65d9e3283e2827ca967a5eb7324bcdeb7a23eb4
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-19 12:38:41 +0800

    Output and Newline characters

---
+++ newline-character.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "Number 1"; 
+    std::cout << "Number 2" << std::endl; // endl is the newline character
+    std::cout << "Number 3";
+    
+    
+}
