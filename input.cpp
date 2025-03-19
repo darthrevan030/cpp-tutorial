30d8ee1556019e8edb04f7463c6e7c8824b9a75c
darth.revan.030 <smartsamarth03@gmail.com>
2025-03-19 15:47:37 +0800

    Enhance user input program to include age input and output statement

--- input.cpp
+++ input.cpp
@@ -6,11 +6,14 @@ int main(){
 
     std::string name;
 
+    // taking str as input
     std::cout << "Enter name: " << std::endl;
     std::cin >> name;
 
+    // taking int as input
     std::cout << "Enter age: " << std::endl;
     std::cin >> age;
 
+    // using the inputs from the user, making an output statement
     std::cout << "" << name << " is " << age << " years old." << std::endl;
 }
